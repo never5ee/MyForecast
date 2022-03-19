67d01db87c1789eb5b4c55f5191d714ce84c9ce7
never5ee <coldmallboro@gmail.com>
2022-03-19 14:35:38 +0300

    Creating Api and Model

---
+++ app/src/main/java/com/example/myforecast/utils/Constants.kt
@@ -0,0 +1,18 @@
+package com.example.myforecast.utils
+
+object Constants {
+
+    //For Api
+    const val METRIC = "metric"
+    const val APPID  = "eda09d8ce11bc18c064abbdbfb011e73"
+    const val GENERIC_ERROR = "Что-то пошло не так, попробуйте еще раз!"
+
+    //Constants for Room
+    const val DATABASE_VERSION = 1
+    const val DATABASE_NAME = "Forecast"
+
+    //Constants for Retrofit
+    const val BASE_ULR = "https://openweathermap.org/"
+
+
+}
